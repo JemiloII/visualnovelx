8949854c25a902bd3fb56d7706255c87df7249f2
Brian M. Jemilo II <jemiloii@jemiloii.com>
2014-05-01 20:41:44 -0500

    SQL File For Recreating users table

---
+++ databases/users.sql
@@ -0,0 +1,24 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `users_id` int(11) NOT NULL AUTO_INCREMENT,
+  `users_display_name` varchar(50) NOT NULL,
+  `users_login_name` varchar(50) NOT NULL,
+  `users_email` varchar(50) NOT NULL,
+  `users_password` varchar(50) NOT NULL,
+  `users_account_type` varchar(25) NOT NULL,
+  `users_signup_date` date NOT NULL,
+  `users_last_login_date` date NOT NULL,
+  PRIMARY KEY (`users_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
