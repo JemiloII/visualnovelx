90cdc289341e3c35d102fed151db15ecb56bb49d
Brian M. Jemilo II <jemiloii@jemiloii.com>
2014-05-01 19:45:14 -0500

    added a route for /users/:name where :name = users_login_name

--- models/users.js
+++ models/users.js
@@ -31,24 +31,41 @@ module.exports.edit = function(app, req, res){
 	console.log('Users: Edit > Model');
 	mysql_connect();
 	users = [];
-	account = "";
+	account = [];
 
 	connection.connect();
 	connection.query('SELECT * FROM users WHERE users_id="'+req.params.id+'"', function(err, rows, fields) {
 		if (err) throw err;
 
 		for(var i=0; i< rows.length; i++){
-			console.log(i);
 			users[i] = rows[i];
-			account = rows[i].users_account_type;
 		}
+
+		var account = users[0].users_account_type;
 	});
 	connection.end();
 };
 
 module.exports.show = function(app, req, res){
 	console.log('Users: Show > Model');
-	return false;
+	mysql_connect();
+	users = [];
+	account = [];
+	if(req.params.id){
+		query='SELECT * FROM users WHERE users_id="'+req.params.id+'"';
+	}else if(req.params.name){
+		query= 'SELECT * FROM users WHERE users_login_name="'+req.params.name+'"';
+	}
+
+	connection.connect();
+	connection.query(query, function(err, rows, fields) {
+		if (err) throw err;
+
+		for(var i=0; i< rows.length; i++){
+			users[i] = rows[i];
+		}
+	});
+	connection.end();
 };
 
 module.exports.destroy = function(app, req, res){
